a657f20d098ac0933a2c01ff535484fda8d5eb22
Ante Budimir <abudimir85@gmail.com>
2021-07-05 09:02:26 +0200

    new function

--- js/main.js
+++ js/main.js
@@ -21,6 +21,18 @@ binanceSocket.addEventListener('message', (event) => {
 	// convert responseValues to an array
 	const data = Object.values(responseValues.data);
 	// console.log(data);
+	// price values are located at data[4]
+
+	//
+	const currencies = [
+		'ETHUSDT',
+		'BTCUSDT',
+		'ADAUSDT',
+		'MATICUSDT',
+		'MANAUSDT',
+		'SANDUSDT',
+		'VETUSDT',
+	];
 
 	// Output paths
 	const ethPrice = document.querySelector('#ethPrice'),
@@ -31,45 +43,25 @@ binanceSocket.addEventListener('message', (event) => {
 		sandPrice = document.querySelector('#sandPrice'),
 		vetPrice = document.querySelector('#vetPrice');
 
-	// ETH
-	if (data[2] === 'ETHUSDT') {
-		const ETHUSDT = `ETH/USDT ${parseFloat(data[4]).toFixed(2)}`;
-		ethPrice.textContent = ETHUSDT;
-	}
+	const prices = [
+		ethPrice,
+		btcPrice,
+		adaPrice,
+		maticPrice,
+		manaPrice,
+		sandPrice,
+		vetPrice,
+	];
 
-	// BTC
-	if (data[2] === 'BTCUSDT') {
-		const BTCUSDT = `BTC/USDT ${parseFloat(data[4]).toFixed(2)}`;
-		btcPrice.textContent = BTCUSDT;
-	}
+	// Process all currencies
+	let index = -1;
 
-	// ADA
-	if (data[2] === 'ADAUSDT') {
-		const ADAUSDT = `ADA/USDT ${parseFloat(data[4]).toFixed(4)}`;
-		adaPrice.textContent = ADAUSDT;
-	}
+	currencies.forEach((currency) => {
+		index++;
 
-	// MATIC
-	if (data[2] === 'MATICUSDT') {
-		const MATICUSDT = `MATIC/USDT ${parseFloat(data[4]).toFixed(4)}`;
-		maticPrice.textContent = MATICUSDT;
-	}
-
-	// MANA
-	if (data[2] === 'MANAUSDT') {
-		const MANAUSDT = `MANA/USDT ${parseFloat(data[4]).toFixed(4)}`;
-		manaPrice.textContent = MANAUSDT;
-	}
-
-	// SAND
-	if (data[2] === 'SANDUSDT') {
-		const SANDUSDT = `SAND/USDT ${parseFloat(data[4]).toFixed(4)}`;
-		sandPrice.textContent = SANDUSDT;
-	}
-
-	// VET
-	if (data[2] === 'VETUSDT') {
-		const VETUSDT = `VET/USDT ${parseFloat(data[4]).toFixed(4)}`;
-		vetPrice.textContent = VETUSDT;
-	}
+		if (data[2] === currency) {
+			const currencyValue = `${currency} ${parseFloat(data[4]).toFixed(4)}`;
+			prices[index].textContent = currencyValue;
+		}
+	});
 });
